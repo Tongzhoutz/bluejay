cbd86a6505f17273d45f5b1653ed95eb2173f660
tongzhoutz <tzhou11@jhu.edu>
2022-12-31 11:31:21 -0500

    feat: 🎸 Test REST API using jason-server

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # BlueJay
 An active platform for Hopkins students to buy and sell second-hand items.
 
+## Send request
+- request: get post put patch delete
+- service: const getUser = (params) => get('/usr', params).then((res) => {
+    return res;
+});
+
+## Test REST API: 
+- Use json-server on Node.
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,42 +1,46 @@
 import React from 'react';
-import {Button, Input, Form, Dialog} from 'antd-mobile';
+import { Button, Input, Form, Dialog } from 'antd-mobile';
 import classes from './Login.module.css';
+import { loginService } from '../services/login';
+const Login = () => {
+    const [form] = Form.useForm();
+    const onSubmit = async () => {
+        const values = form.getFieldsValue();
+        const res = await loginService(values.username, values.password);
+        if (res && res.length > 0) {
+            Dialog.alert({
+              content: "Login successful!",
+            });
+            return;
+        }
+        Dialog.alert({
+            content: "Login Failed!"
+        });
+    };
 
-function Login() {
-  const [form] = Form.useForm();
-  const onSubmit = () => {
-    const values = form.getFieldsValue();
-    Dialog.alert({
-      content: JSON.stringify(values),
-    });
-  };
+    const initialValues = {
+        username: 'anonymous', 
+        password: '12345',
+    }
 
-  const initialValues = {
-    username2: 'hahaha',
-    password2: '12345',
-  };
+    return (
+        <div className={classes.login}>
+            <Form form={form} layout="horizontal" mode="card"
+                  initialValues={initialValues} 
+                  footer={
+                    <Button block color='primary' onClick={onSubmit} size='large'>
+                        Log In
+                    </Button>}
+            >
+                <Form.Item label="Username" name="username">
+                    <Input placeholder="please enter your username" type='text' clearable/>
+                </Form.Item>
+                <Form.Item label="Password" name="password">
+                    <Input placeholder="please enter your password" type='password' clearable/>
+                </Form.Item>
+            </Form>
+        </div>
 
-  return (
-    <div className={classes.login} >
-      <Form
-        form={form}
-        layout="horizontal"
-        mode="card"
-        initialValues={initialValues}
-        footer={(
-          <Button block color='primary' onClick={onSubmit} size='large'>
-            Log In
-          </Button>
-          )}
-      >
-        <Form.Item label="Username" name="username2">
-          <Input placeholder="please input" type="text" clearable />
-        </Form.Item>
-        <Form.Item label="Password" name="password2">
-          <Input placeholder="please input" type="passwowrd" clearable/>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+    )
 }
 export default Login;
