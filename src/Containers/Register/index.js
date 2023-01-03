cd7791e2514cd03979a958f931b38bca043da53a
tongzhoutz <tzhou11@jhu.edu>
2023-01-03 09:50:36 -0500

    refactor: 💡 add customized component: DatePickerInput

--- src/Containers/Register/index.js
+++ src/Containers/Register/index.js
@@ -1,20 +1,56 @@
 
-import { Input, Button } from 'antd-mobile';
+import { useState } from 'react';
+import { Input, Button, Form } from 'antd-mobile';
 import styles from './index.module.css';
 import DatePickerInput from '@components/DatePickerInput';
 import Header from '@components/Header';
 
+const ACCOUNT_TYPE = {
+    TEL: 'TEL',
+    EMAIL: 'EMAIL',
+};
+
 const Register = () => {
+    const [formData] = useState({
+        name: '',
+        tel: '',
+        email: '',
+        dob: '20220202',
+    });
+
+    const [accountType, setAccountType] = useState(ACCOUNT_TYPE.TEL);
+    const onAccountTypeChange = () => {
+        if (accountType === ACCOUNT_TYPE.TEL){
+            setAccountType(ACCOUNT_TYPE.EMAIL);
+            return;
+        } 
+        setAccountType(ACCOUNT_TYPE.TEL);
+    }
+
     return <div>
         <Header/>
         <div className={styles.form}>
             <div className={styles.formTitle}>Create your account</div>
-            <Input placeholder="Name" className={styles.input}/>
-            <Input placeholder="Phone number" className={styles.input}/>
-            <div className={styles.changeEmailBtn}>Use email instead</div>
-            <div className={styles.DoB}>Date of Birth</div>
-            <div>This will not be shown publicly. Confirm your own age, even if this accouont is for a business, a pet, or something else.</div>
-            <DatePickerInput/>
+            <Form initialValues={formData} className={styles.formContainer}>
+                <Form.Item name='name'>
+                    <Input placeholder="Name" className={styles.input}/>
+                </Form.Item>
+
+                {accountType === ACCOUNT_TYPE.TEL && <Form.Item name = 'TEL' > <Input placeholder="Phone number" className={styles.input}/>
+                </Form.Item>
+                }
+                {accountType === ACCOUNT_TYPE.EMAIL && <Form.Item name = 'EMAIL'> <Input placeholder="Email" className={styles.input}/>
+                </Form.Item>
+                }
+                <div className={styles.changeEmailBtn} onClick={onAccountTypeChange}>
+                    {accountType === ACCOUNT_TYPE.EMAIL ? "Use email instead" : "Use phone number"}
+                </div>
+                <div className={styles.DoB}>Date of Birth</div>
+                <div>This will not be shown publicly. Confirm your own age, even if this accouont is for a business, a pet, or something else.</div>
+                <Form.Item name='dob'>
+                    <DatePickerInput/>
+                </Form.Item>
+            </Form>
         </div>
         <div className={styles.footer}>
             <Button className={styles.footerBtn}>Next</Button>
--- src/components/DatePickerInput/index.js
+++ src/components/DatePickerInput/index.js
@@ -1,11 +1,44 @@
 import styles from './index.module.css';
 import datePickerIcon from '@assets/datePicker-icon.svg';
+import { DatePicker } from 'antd-mobile';
+import { useState } from 'react';
+import moment from 'moment';
+import PropTypes from 'prop-types';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default  () => (<div className={styles.birthdayInput}>
+const DatePickerInput =  ({
+    value, 
+    onChange
+}) => {
+const [visible, setVisible] = useState(false);
+const [curDate, setCurDate] = useState();
+
+const onClickDatePicker = () => {
+    setVisible(true);
+}
+return (
+    <>
+        <DatePicker
+            title='Date of Birth'
+            visible={visible}
+            onClose={() => {
+                setVisible(false)
+            }}
+            onConfirm={ (val) => {
+                onChange(val);
+            }}
+        />
+            
+            <div className={styles.birthdayInput} onClick={onClickDatePicker}>
                 <div className={styles.dobPlaceHolder}>date of birth</div>
                 <div>
-                    <span className={styles.yymmdd}>YY/MM/DD</span>
+                    <span className={styles.yymmdd}>{value ? moment(value).format('YYYY-MM-DD') : 'YY/MM/DD'  }</span>
                     <img className={styles.datePickerIcon} src={datePickerIcon} />
                 </div>
-            </div>);
+            </div>
+    </>)};
+DatePickerInput.propTypes = {
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+}
+export  default DatePickerInput;
